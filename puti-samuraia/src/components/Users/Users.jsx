a704d0ce8242501d23aa226c271413a7ec7632e5
viorel556 <viorel.harabaru@gmail.com>
2023-10-02 14:44:33 +0300

    installing axios, first GET request to a server

--- puti-samuraia/src/components/Users/Users.jsx
+++ puti-samuraia/src/components/Users/Users.jsx
@@ -1,46 +1,16 @@
 import React from "react"
 import UsersContainer from "./UsersContainer";
 import styles from './users.module.css';
+import axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
-    // THIS SHIT HAS TO BE CHANGED
-    // JUST POPULATE users array in "users-reducer.js" with the bellow array;
-    // THAT SHOULD DO IT;
     if (props.users.length === 0 ) {
-        props.loadUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: "https://images11.graziamagazine.ru/upload/img_cache/326/32663d820526714720dce74265bee92a_ce_2832x1486x0x120.jpg",
-                    followed: false,
-                    fullName: "Dmitrii",
-                    status: "I am a Boss!",
-                    location: { city: "Minsk", country: "Belarus" },
-                },
-
-                {
-                    id: 2,
-                    photoUrl: "https://images11.graziamagazine.ru/upload/img_cache/326/32663d820526714720dce74265bee92a_ce_2832x1486x0x120.jpg",
-                    fullName: "Felicia",
-                    followed: false,
-                    status: "I am a Freelancer!",
-                    location: { city: "Chisinau", country: "Moldova" },
-                },
-
-                {
-                    id: 3,
-                    photoUrl: "https://images11.graziamagazine.ru/upload/img_cache/326/32663d820526714720dce74265bee92a_ce_2832x1486x0x120.jpg",
-                    followed: true,
-                    fullName: "Mozgan",
-                    status: "I am a smort!",
-                    location: { city: "Stutgart", country: "Germany" },
-                },
-            ]
-        );
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+            props.loadUsers(response.data.items);
+        });
     }
-
-
 
     return (
         <div>
@@ -48,7 +18,7 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                         <span>
                             <div>
-                                <img src={u.photoUrl} className={styles.userPhoto}/>
+                                <img src={u.photos.small != null ? u.photos.small: userPhoto   } className={styles.userPhoto}/>
                             </div>
 
                             <div>
@@ -61,12 +31,12 @@ let Users = (props) => {
 
                         <span>
                             <span>
-                                <div>{u.fullName}</div> <div>{u.status}</div>
+                                <div>{u.name}</div> <div>{u.status}</div>
                             </span>
 
                             <span>
-                                <div>{u.location.country}</div>
-                                <div>{u.location.city}</div>
+                                <div>{"u.location.country"}</div>
+                                <div>{"u.location.city"}</div>
                             </span>
                         </span>
                     </div>
