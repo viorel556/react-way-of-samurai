5d27ac3bcd6bfcaa94bbb2fa71a154630b5c09e3
viorel556 <viorel.harabaru@gmail.com>
2023-10-04 13:08:46 +0300

    small refactoring - Side Effect Principle

--- puti-samuraia/src/components/Users/Users.jsx
+++ puti-samuraia/src/components/Users/Users.jsx
@@ -6,14 +6,19 @@ import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
-    if (props.users.length === 0 ) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
-            props.loadUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0 ) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+                props.loadUsers(response.data.items);
+            });
+        }
     }
+
+
 
     return (
         <div>
+            <button onClick={ getUsers }> Get Users </button>
             {
                 props.users.map(u => <div key={u.id}>
                         <span>
