3d147bf404ad6e9ee78bc2809f275ba680ca2dc5
viorel556 <viorel.harabaru@gmail.com>
2023-11-14 23:01:07 +0200

    Set-up: Redux ultra usefull ADD-ON for chrome; git status

--- puti-samuraia/src/redux/redux-store.js
+++ puti-samuraia/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -20,8 +20,14 @@ let reducers = combineReducers(
     }
 );
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+// HERE WE CREATE THE STORE:
 
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers( applyMiddleware(thunkMiddleware) )
+)
+
+window.__store__ = store;
 
 export default store;
